a1dd9b9ff3f0f3fec9f61b9a1fca03a439f39830
DarkenSoda <mahmoudadel200190@yahoo.com>
2024-03-25 16:04:43 +0200

    Windows API

---
+++ main.cpp
@@ -0,0 +1,66 @@
+#ifndef UNICODE
+#define UNICODE
+#endif
+
+#ifndef _UNICODE
+#define _UNICODE
+#endif
+
+#include <windows.h>
+#include <cwchar>
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine, int nCmdShow) {
+    // Register the window class.
+    LPCWCHAR CLASS_NAME = L"Computer Graphics";
+
+    WNDCLASS wc = { };
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+
+    RegisterClass(&wc);
+
+    HWND hwnd = CreateWindowEx(
+        0,
+        CLASS_NAME,
+        CLASS_NAME,
+        WS_OVERLAPPEDWINDOW,
+
+        // position and size
+        CW_USEDEFAULT, CW_USEDEFAULT, 800, 600,
+
+        NULL,       // Parent window    
+        NULL,       // Menu
+        hInstance,  // Instance handle
+        NULL        // Additional application data
+    );
+
+    if (hwnd == NULL) {
+        return 0;
+    }
+
+    ShowWindow(hwnd, nCmdShow);
+
+    // Run the message loop.
+    MSG msg = { };
+    while (GetMessage(&msg, NULL, 0, 0) > 0) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return 0;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch (uMsg) {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    }
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
---
+++ src/Vector2D.cpp
@@ -0,0 +1,9 @@
+#include <Vector2D.h>
+
+Vector2D::Vector2D(int x, int y) : _x(x), _y(y) {}
+
+int Vector2D::X() { return _x; }
+
+int Vector2D::Y() { return _y; }
+
+Vector2D::~Vector2D() {}
