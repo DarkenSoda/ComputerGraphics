5bb63887910701fae1e58feac05d45638cc74a27
Yassin Tarek Helmy <102481887+YassinTarekHelmy@users.noreply.github.com>
2024-05-05 23:57:20 +0300

    I added the PolygonFill But still there are things to do

--- src/ShapeTools/PolygonTool.cpp
+++ src/ShapeTools/PolygonTool.cpp
@@ -1,4 +1,10 @@
 #include "PolygonTool.h"
+#include <tuple>
+#include <vector>
+#include <set>
+#include <Utils.h>
+
+typedef std::vector<std::set<EdgeData>> EdgeTable;
 
 void PolygonTool::drawPolygon(HDC hdc, Vector2D* point, int size, COLORREF color) {
     for (int i = 1; i < size; i++) {
@@ -8,9 +14,76 @@ void PolygonTool::drawPolygon(HDC hdc, Vector2D* point, int size, COLORREF color
     LineDrawer::bresenhamLine(hdc, &point[size - 1], &point[0], color);
 }
 
-void PolygonTool::fillPolygon(HDC, Vector2D*, int, COLORREF) {
+
+void PolygonTool::fillPolygon(HDC hdc, Vector2D* polygonList, int size, COLORREF color) {
+    EdgeTable edgeTable(600);
+    InitializeEdgeTable(edgeTable, polygonList, size);
+    FillPolygonUtil(hdc, edgeTable, color);
+    
+
+}
+
+void PolygonTool::FillPolygonUtil(HDC hdc, EdgeTable& edgeTable, COLORREF color) {
+    for (int i = 0; i < edgeTable.size(); i++) {
+        for (auto it = edgeTable[i].begin(); it != edgeTable[i].end(); it++) {
+            int xMin = it->xMin;
+            int yMax = it->yMax;
+            double slope = it->slope;
+
+            int x1 = xMin;
+            it++;
+            if (it == edgeTable[i].end()) {
+                break;
+            }
+            int x2 = it->xMin;
+            double slope2 = it->slope;
+
+            for (int y = i; y < yMax; y++) {
+                LineDrawer::bresenhamLine(hdc, new Vector2D(x1, y), new Vector2D(x2, y), color);
+                x1 += slope;
+                x2 += slope2;
+            }
+        }
+
+        for (auto it = edgeTable[i].begin(); it != edgeTable[i].end();) {
+            if (it->yMax == i) {
+                it = edgeTable[i].erase(it);
+            }
+            else {
+                it++;
+            }
+        }
+
+        //TODO:
+        // Update the vaues of XMin.
+        // Append the current Set to the Set in the next Iteration.
+
+    }
+    
     
 }
+
+void PolygonTool::InitializeEdgeTable(EdgeTable& edgeTable, Vector2D* polygonList, int size) {
+    for (int i = 0; i < size; i++) {
+        Vector2D start = polygonList[i];
+        Vector2D end = polygonList[(i + 1) % size];
+
+        if (start.Y() == end.Y()) {
+            continue;
+        }
+        if (start.Y() > end.Y()) {
+            Utils::swap(start, end);
+        }
+
+        int dx = end.X() - start.X();
+        int dy = end.Y() - start.Y();
+        double slope = (double)dx / dy;
+
+        EdgeData edgeData(start.X(), end.Y(), slope);
+        edgeTable[start.Y()].insert(edgeData);
+    }
+}
+
 
 void PolygonTool::handleMsg(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lp) {
     switch (uMsg) {
--- src/ShapeTools/PolygonTool.h
+++ src/ShapeTools/PolygonTool.h
@@ -5,15 +5,30 @@
 #include "vector"
 #include "LineDrawer.h"
 
+
+typedef std::vector<std::set<EdgeData>> EdgeTable;
 class PolygonTool : public Tool {
 private:
     std::vector<Vector2D> pointList;
     bool polygonExists = false;
     bool canFill = false;
+    static void InitializeEdgeTable(EdgeTable&, Vector2D*, int);
+    static void FillPolygonUtil(HDC, EdgeTable&, COLORREF);
 public:
     static void drawPolygon(HDC, Vector2D*, int, COLORREF);
     static void fillPolygon(HDC, Vector2D*, int, COLORREF);
     virtual void handleMsg(HWND, UINT, WPARAM, LPARAM) override;
+};
+
+class EdgeData {
+public:
+    int xMin;
+    int yMax;
+    double slope;
+    EdgeData(int xMin, int yMax, double slope) : xMin(xMin), yMax(yMax), slope(slope) {}
+    int operator<(const EdgeData& other) {
+        return xMin < other.xMin;
+    }
 };
 
 #endif
